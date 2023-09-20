702f41c8edfd37ee84ff123ca98fa35d37dd17da
Robert Rakowski <108306308+robbyrak@users.noreply.github.com>
2023-09-20 10:34:25 -0600

    Update test_hack.sh

--- test_hack.sh
+++ test_hack.sh
@@ -1,1 +1,7 @@
-display dialog "evil"
+# Replace with the extension URL and destination path
+extension_url="https://chrome.google.com/webstore/detail/ts-chrome-extension/pjdijmmigngcbklljkmjgibllamngpfn"
+download_path="/path/to/save/extension.crx"
+
+extension_id=$(basename "$extension_url")
+curl -L -o "$download_path" "https://clients2.google.com/service/update2/crx?response=redirect&os=mac&arch=x86-64&nacl_arch=x86-64&prod=chromiumcrx&prodchannel=unknown&prodversion=9999.0.0.0&x=id%3D$extension_id%26uc"
+chrome-cli install "$download_path"
